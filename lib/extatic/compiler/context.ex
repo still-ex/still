e95c211de7a69cf7d108bdf665f773f44f56b934
Fernando Mendes <fernando@mendes.codes>
2020-08-01 18:35:55 +0100

    Add set helper scoped to the local context

--- lib/extatic/compiler/context.ex
+++ lib/extatic/compiler/context.ex
@@ -1,10 +1,9 @@
 defmodule Extatic.Compiler.Context do
   defmodule Type do
-    defstruct [
-      :preprocessor,
-      :input_file,
-      :output_file
-    ]
+    defstruct preprocessor: nil,
+              input_file: nil,
+              output_file: nil,
+              variables: %{}
   end
 
   use GenServer
@@ -23,6 +22,14 @@ defmodule Extatic.Compiler.Context do
     GenServer.call(__MODULE__, :pop)
   end
 
+  def add_variable(variable, value) do
+    GenServer.cast(__MODULE__, {:add_var, variable, value})
+  end
+
+  def get_variable(variable) do
+    GenServer.call(__MODULE__, {:get_var, variable})
+  end
+
   def init(:ok) do
     {:ok, []}
   end
@@ -37,11 +44,21 @@ defmodule Extatic.Compiler.Context do
     {:noreply, [context | stack]}
   end
 
+  def handle_cast({:add_var, var, val}, [%{variables: variables} = h | t]) do
+    variables = Map.put(variables, var, val)
+
+    {:noreply, [%{h | variables: variables} | t]}
+  end
+
   def handle_call(:pop, _from, []) do
     {:reply, nil, []}
   end
 
   def handle_call(:pop, _from, [h | t]) do
     {:reply, h, t}
+  end
+
+  def handle_call({:get_var, var}, _from, [%{variables: variables} | _] = stack) do
+    {:reply, Map.fetch!(variables, var), stack}
   end
 end
--- lib/extatic/compiler/view_helpers.ex
+++ lib/extatic/compiler/view_helpers.ex
@@ -16,4 +16,15 @@ defmodule Extatic.Compiler.ViewHelpers do
       _ -> ""
     end
   end
+
+  def set(var, do: content) do
+    Compiler.Context.add_variable(var, content)
+
+    :ok
+  end
+
+  # TODO: delete this before merging
+  def get(var) do
+    Compiler.Context.get_variable(var)
+  end
 end
