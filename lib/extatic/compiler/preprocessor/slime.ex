a75c8cb73d4012742bf45204b7ae98564365307a
Fernando Mendes <fernando@mendes.codes>
2020-08-26 18:15:28 +0100

    Refactor renderer

--- lib/extatic/compiler/preprocessor/slime.ex
+++ lib/extatic/compiler/preprocessor/slime.ex
@@ -3,7 +3,11 @@ if Code.ensure_loaded?(Slime) do
     require Logger
     require Slime
 
-    alias Extatic.Compiler.Preprocessor
+    alias Extatic.Compiler.{
+      Preprocessor,
+      Preprocessor.Slime.Renderer,
+      Preprocessor.Slime.ContentTag
+    }
 
     @behaviour Preprocessor
 
@@ -21,117 +25,26 @@ if Code.ensure_loaded?(Slime) do
 
     @impl true
     def content_tag(tag, content, opts, variables) do
-      slim = Preprocessor.Slime.ContentTag.render(tag, content, opts)
+      do_content_tag(tag, content, opts, variables)
+    rescue
+      e in Slime.TemplateSyntaxError ->
+        raise Preprocessor.SyntaxError,
+          message: e.message,
+          line_number: e.line_number,
+          line: e.line,
+          column: e.column
+    end
 
-      snippet_renderer(slim, variables)
+    defp do_content_tag(tag, content, opts, variables) do
+      slim = ContentTag.render(tag, content, opts)
+
+      Renderer.create_snippet(slim, variables)
       |> apply(:render, variables |> Enum.into(%{}) |> Map.values())
     end
 
     defp do_render(content, variables) do
-      variables[:file_path]
-      |> file_path_to_module_name()
-      |> create_slime_view_renderer(content, variables)
+      Renderer.create(content, variables)
       |> apply(:render, variables |> Enum.into(%{}) |> Map.values())
-    end
-
-    defp file_path_to_module_name(file) do
-      name =
-        Path.split(file)
-        |> Enum.reject(&Enum.member?(["/", ".slime"], &1))
-        |> Enum.map(&String.replace(&1, ".slime", ""))
-        |> Enum.map(&String.replace(&1, "_", ""))
-        |> Enum.map(&String.capitalize/1)
-
-      Module.concat([Preprocessor.Slime | name])
-    end
-
-    defp create_slime_view_renderer(name, content, variables) do
-      compiled = compile_slime(content, variables)
-      args = get_args(variables)
-
-      module_variables =
-        variables
-        |> ensure_current_context()
-        |> ensure_preprocessor()
-
-      ast =
-        quote do
-          @compile :nowarn_unused_vars
-
-          require Slime
-          require EEx
-
-          use Extatic.Compiler.ViewHelpers, unquote(Macro.escape(module_variables))
-
-          def render(unquote_splicing(args)) do
-            unquote(compiled)
-          end
-        end
-
-      with {:module, mod, _, _} <- Module.create(name, ast, Macro.Env.location(__ENV__)) do
-        mod
-      end
-    end
-
-    defp get_args(variables) do
-      info = [file: __ENV__.file, line: __ENV__.line]
-
-      Enum.map(Map.new(variables) |> Map.keys(), fn arg ->
-        {arg, [line: info[:line]], nil}
-      end)
-    end
-
-    defp compile_slime(content, variables) do
-      info = [file: __ENV__.file, line: __ENV__.line]
-
-      Slime.Renderer.precompile(content)
-      |> EEx.compile_string(info)
-      |> ignore_unused_variables(variables)
-    end
-
-    defp ignore_unused_variables(ast, variables) do
-      Enum.reduce(variables, ast, fn {k, _v}, memo ->
-        quote do
-          _ = var!(unquote(Macro.var(k, __MODULE__)))
-          unquote(memo)
-        end
-      end)
-    end
-
-    defp snippet_renderer(content, variables) do
-      compiled = compile_slime(content, variables)
-      args = get_args(variables)
-
-      ast =
-        quote do
-          @compile :nowarn_unused_vars
-
-          require Slime
-          require EEx
-
-          use Extatic.Compiler.ViewHelpers, unquote(Macro.escape(variables))
-
-          def render(unquote_splicing(args)) do
-            unquote(compiled)
-          end
-        end
-
-      with {:module, mod, _, _} <-
-             Module.create(
-               Extatic.Compiler.Preprocessor.Slime.SnippetRenderer,
-               ast,
-               Macro.Env.location(__ENV__)
-             ) do
-        mod
-      end
-    end
-
-    defp ensure_current_context(variables) do
-      Keyword.put_new(variables, :current_context, variables[:file_path])
-    end
-
-    defp ensure_preprocessor(variables) do
-      Keyword.put_new(variables, :preprocessor, __MODULE__)
     end
   end
 end
