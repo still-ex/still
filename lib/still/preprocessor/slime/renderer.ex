69e95a6f4a65ef43a1b419d2cb0caec28ace831d
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2020-09-24 11:48:44 +0100

    Slime is no longer optional
    We need Slime because our development layout is written in Slime. It
    also makes sense to include Slime by default because this goes with our
    philosophy to require as little configuration as possible to get
    started.

--- lib/still/preprocessor/slime/renderer.ex
+++ lib/still/preprocessor/slime/renderer.ex
@@ -1,23 +1,21 @@
-if Code.ensure_loaded?(Slime) do
-  defmodule Still.Preprocessor.Slime.Renderer do
-    use Still.Preprocessor.Renderer,
-      extensions: [".slime"],
-      preprocessor: Still.Preprocessor.Slime
+defmodule Still.Preprocessor.Slime.Renderer do
+  use Still.Preprocessor.Renderer,
+    extensions: [".slime"],
+    preprocessor: Still.Preprocessor.Slime
 
-    @impl true
-    def compile(content, _variables) do
-      info = [file: __ENV__.file, line: __ENV__.line]
+  @impl true
+  def compile(content, _variables) do
+    info = [file: __ENV__.file, line: __ENV__.line]
 
-      Slime.Renderer.precompile(content)
-      |> EEx.compile_string(info)
-    end
+    Slime.Renderer.precompile(content)
+    |> EEx.compile_string(info)
+  end
 
-    @impl true
-    def ast do
-      quote do
-        require EEx
-        require Slime
-      end
+  @impl true
+  def ast do
+    quote do
+      require EEx
+      require Slime
     end
   end
 end
