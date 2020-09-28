ad71ec90a33e21309a8ba198d79631c229eaa176
Fernando Mendes <fernando@mendes.codes>
2020-09-28 19:52:54 +0100

    Fix error in template
    We're getting a syntax error when generating a new project

--- templates/config/config.exs
+++ templates/config/config.exs
@@ -1,5 +1,5 @@
 import Config
 
 config :still,
-  input: Path.join(Path.dirname(__DIR__), "priv/site")
+  input: Path.join(Path.dirname(__DIR__), "priv/site"),
   output: Path.join(Path.dirname(__DIR__), "_site")
