8d581fe2c943d2553274d224786e8bd6211e676d
Gabriel Poça <gabrielpoca@gmail.com>
2020-10-06 22:52:38 +0100

    ups

--- lib/still/compiler/incremental/node.ex
+++ lib/still/compiler/incremental/node.ex
@@ -28,7 +28,7 @@ defmodule Still.Compiler.Incremental.Node do
   alias Still.Compiler
   alias __MODULE__.Compile
 
-  @default_compilation_timeout 25_000
+  @default_compilation_timeout 5_000
 
   def start_link(file: file) do
     GenServer.start_link(__MODULE__, %{file: file}, name: file |> String.to_atom())
