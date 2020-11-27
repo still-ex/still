83111a1b4987cbd6981220c88b923eb67a9f7183
Gabriel Poça <gabrielpoca@gmail.com>
2020-11-27 22:19:44 +0000

    Move generator to different project

---
+++ installer/README.md
@@ -0,0 +1,20 @@
+# mix still.new
+
+**TODO: Add description**
+
+## Installation
+
+If [available in Hex](https://hex.pm/docs/publish), the package can be installed
+by adding `still_new` to your list of dependencies in `mix.exs`:
+
+```elixir
+def deps do
+  [
+    {:still_new, "~> 0.1.0"}
+  ]
+end
+```
+
+Documentation can be generated with [ExDoc](https://github.com/elixir-lang/ex_doc)
+and published on [HexDocs](https://hexdocs.pm). Once published, the docs can
+be found at [https://hexdocs.pm/still_new](https://hexdocs.pm/still_new).
---
+++ installer/mix.exs
@@ -0,0 +1,36 @@
+defmodule Still.New.MixProject do
+  use Mix.Project
+
+  @version "0.1.0"
+
+  def project do
+    [
+      app: :still_new,
+      version: @version,
+      elixir: "~> 1.10",
+      start_permanent: Mix.env() == :prod,
+      aliases: aliases(),
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [extra_applications: [:logger]]
+  end
+
+  defp deps do
+    []
+  end
+
+  defp aliases do
+    [build: [&build_releases/1]]
+  end
+
+  defp build_releases(_) do
+    Mix.Tasks.Compile.run([])
+    Mix.Tasks.Archive.Build.run([])
+    Mix.Tasks.Archive.Build.run(["--output=still_new.ez"])
+    File.rename("still_new.ez", "./archives/still_new.ez")
+    File.rename("still_new-#{@version}.ez", "./archives/still_new-#{@version}.ez")
+  end
+end
