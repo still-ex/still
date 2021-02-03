2e67b8ad13af67b9e3ba50a9709ebad55ebef21c
Fernando Mendes <fernando@mendes.codes>
2021-02-03 18:01:00 +0000

    Add logo flickering animation

--- config/dev.exs
+++ config/dev.exs
@@ -5,4 +5,4 @@ config :still,
   base_url: "http://localhost:3000",
   input: Path.join(Path.dirname(__DIR__), "priv/site"),
   output: Path.join(Path.dirname(__DIR__), "_site"),
-  pass_through_copy: [~r/.*jpe?g/, "subvisual.png", "fonts"]
+  pass_through_copy: [~r/.*jpe?g/, "subvisual.png", "fonts", "images"]
