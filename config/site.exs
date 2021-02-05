ec1a9b184acbb2e419725e15ca2f81c09c5d7cdd
Fernando Mendes <fernando@mendes.codes>
2021-02-05 16:04:57 +0000

    Pass-through copy CNAME

--- config/site.exs
+++ config/site.exs
@@ -2,4 +2,11 @@ import Config
 
 config :still,
   url_fingerprinting: true,
-  base_url: "https://stillstatic.io"
+  base_url: "https://stillstatic.io",
+  pass_through_copy: [
+    ~r/.*jpe?g/,
+    "subvisual.png",
+    "images",
+    "fonts",
+    "CNAME"
+  ]
