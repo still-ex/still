cd09bb03f6001d183d75458b331bd5a26fcb5285
Davide Silva <davidesilva@hey.com>
2021-02-08 16:35:49 +0000

    Fix incremental compiler
    Convert file path to an Atom and check if there is a Process running
    associated to that file path

--- lib/still/compiler/incremental/registry.ex
+++ lib/still/compiler/incremental/registry.ex
@@ -19,7 +19,11 @@ defmodule Still.Compiler.Incremental.Registry do
   Terminates the `Still.Compiler.Incremental.Node` corresponding to the given file name.
   """
   def terminate_file_process(file) do
-    DynamicSupervisor.terminate_child(__MODULE__, Process.whereis(file))
+    file = String.to_atom(file)
+
+    if not is_nil(Process.whereis(file)) do
+      DynamicSupervisor.terminate_child(__MODULE__, Process.whereis(file))
+    end
     :ok
   end
 
