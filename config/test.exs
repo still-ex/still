7a0ad07a189c9fc1c094bef8f4e18d4b7b27b685
Gabriel Poca <mail@gabrielpoca.com>
2021-02-21 23:25:29 +0000

    fix tests

--- config/test.exs
+++ config/test.exs
@@ -3,5 +3,4 @@ import Config
 config :still,
   base_url: "http://localhost:3000",
   input: Path.join(Path.dirname(__DIR__), "test/fixture/site"),
-  output: Path.join(Path.dirname(__DIR__), "._test_site"),
-  compilation_timeout: 45000
+  output: Path.join(Path.dirname(__DIR__), "._test_site")
