6ca9c5411d00a98fd034d4c54fa3c607de08c515
Gabriel Poça <gabrielpoca@users.noreply.github.com>
2021-02-26 14:55:07 +0000

    Fix tests
    I've broken the build with my last commit.

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,11 +1,13 @@
 alias Still.Compiler.{Incremental, Collections, ErrorCache, CompilationStage}
 
 {:ok, _} = Application.ensure_all_started(:timex)
+{:ok, _} = Application.ensure_all_started(:cachex)
 
 {:ok, _pid} = ErrorCache.start_link(%{})
 {:ok, _pid} = Incremental.Registry.start_link(%{})
 {:ok, _pid} = Collections.start_link(%{})
 {:ok, _pid} = CompilationStage.start_link(%{})
+{:ok, _pid} = Cachex.start_link(name: Still.Compiler.ContentCache.cache_name())
 
 ExUnit.start()
 
