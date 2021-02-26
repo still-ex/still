24f814cb016037921baf8731d9c6ce9007cb3975
Miguel Palhas <mpalhas@gmail.com>
2021-02-26 15:46:23 +0000

    Smaller (hopefully more readable?) traverse logic

--- lib/still/compiler/traverse.ex
+++ lib/still/compiler/traverse.ex
@@ -13,38 +13,40 @@ defmodule Still.Compiler.Traverse do
 
     with true <- File.dir?(get_input_path()),
          _ <- File.rmdir(get_output_path()),
-         :ok <- File.mkdir_p(get_output_path()) do
-      do_run()
-    end
-  end
-
-  defp do_run(folder \\ "") do
-    with {:ok, files} <- File.ls(Path.join(get_input_path(), folder)),
-         files <- Enum.reject(files, &String.starts_with?(&1, "_")),
-         _ <- Enum.map(files, &compile_file(Path.join(folder, &1))) do
-      :ok
+         :ok <- File.mkdir_p(get_output_path()),
+         files <- compilable_files() do
+      files
+      |> Enum.map(&compile_file/1)
     end
   end
 
   defp compile_file(file) do
-    if File.dir?(get_input_path(file)) do
-      process_folder(file)
-    else
-      process_file(file)
-    end
-  end
-
-  defp process_folder(folder) do
-    folder
-    |> Incremental.Registry.get_or_create_file_process()
-    |> Incremental.Node.compile()
-    |> case do
-      {:ok, _} -> :ok
-      _ -> do_run(folder)
-    end
+    process_file(file)
   end
 
   defp process_file(file) do
     file |> CompilationStage.compile()
   end
+
+  def compilable_files(rel_path \\ "") do
+    path = Path.join(get_input_path, rel_path)
+
+    cond do
+      partial?(path) ->
+        []
+
+      File.regular?(path) ->
+        [rel_path]
+
+      File.dir?(path) ->
+        File.ls!(path)
+        |> Enum.map(&compilable_files(Path.join(rel_path, &1)))
+
+      true ->
+        []
+    end
+    |> List.flatten()
+  end
+
+  defp partial?(path), do: String.starts_with?(path, "_")
 end
