still-ex/still

commit 987064f35cbfabde33f7ea8a499b5064ccffdc71

[Breaking Change] Template helpers are now pure functions (#133)

lib/still/preprocessor/renderer.ex
@@ -11,7 +11,7 @@ defmodule Still.Preprocessor.Renderer do
         template_helpers: [Your.Module]
 
   The created module implements a `render/0` which will return the result of
-  the `compile/2` call.
+  the `compile/1` call.
 
   The `ast/0` can be used to tap into the AST of the new module and import or
   require any necessary module.
@@ -25,7 +25,7 @@ defmodule Still.Preprocessor.Renderer do
   """
   @type ast :: {atom(), keyword(), list()}
 
-  @callback compile(String.t(), [{atom(), any()}]) :: ast()
+  @callback compile(String.t()) :: ast()
 
   @callback ast() :: ast()
 
@@ -41,7 +41,7 @@ defmodule Still.Preprocessor.Renderer do
       @preprocessor Keyword.fetch!(unquote(opts), :preprocessor)
       @extensions Keyword.fetch!(unquote(opts), :extensions)
 
-      def create(%SourceFile{input_file: input_file, content: content, metadata: metadata}) do
+      def create(%SourceFile{input_file: input_file, content: content, metadata: metadata} = file) do
         metadata[:input_file]
         |> file_path_to_module_name()
         |> create_template_renderer(content, metadata)
@@ -59,12 +59,13 @@ defmodule Still.Preprocessor.Renderer do
       end
 
       defp create_template_renderer(name, content, metadata) do
-        compiled = compile(content, metadata)
+        compiled = compile(content)
 
-        module_metadata =
-          metadata
-          |> ensure_preprocessor()
-          |> Map.to_list()
+        metadata = Map.put(metadata, :env, metadata)
+
+        assigns = [
+          env: metadata
+        ]
 
         renderer_ast =
           if Kernel.function_exported?(__MODULE__, :ast, 0) do
@@ -80,7 +81,7 @@ defmodule Still.Preprocessor.Renderer do
             unquote(user_template_helpers_asts())
             unquote(renderer_ast)
 
-            use Still.Compiler.TemplateHelpers, unquote(Macro.escape(module_metadata))
+            import Still.Compiler.TemplateHelpers
 
             Enum.map(unquote(Macro.escape(metadata)), fn {k, v} ->
               Module.put_attribute(__MODULE__, k, v)

priv/site/_includes/community.slime
@@ -17,7 +17,7 @@
       = link to: "https://github.com/still-ex/still", class: "quote" do
         .mark “
         div
-          = responsive_image("_includes/stars.png", class: "stars")
+          = responsive_image(@env, "_includes/stars.png", class: "stars")
           div Breathtaking, fixed all my issues with static sites.
           div.small -- #{Still.Site.Github.stars} stargazers on GitHub
 

lib/still/preprocessor/eex/renderer.ex
@@ -8,10 +8,8 @@ defmodule Still.Preprocessor.EEx.Renderer do
     preprocessor: Still.Preprocessor.EEx
 
   @impl true
-  def compile(content, _metadata) do
-    info = [file: __ENV__.file, line: __ENV__.line]
-
-    EEx.compile_string(content, info)
+  def compile(content) do
+    EEx.compile_string(content, file: __ENV__.file, line: __ENV__.line)
   end
 
   @impl true

lib/still/preprocessor/slime/renderer.ex
@@ -8,11 +8,9 @@ defmodule Still.Preprocessor.Slime.Renderer do
     preprocessor: Still.Preprocessor.Slime
 
   @impl true
-  def compile(content, _metadata) do
-    info = [file: __ENV__.file, line: __ENV__.line]
-
+  def compile(content) do
     Slime.Renderer.precompile(content)
-    |> EEx.compile_string(info)
+    |> EEx.compile_string(file: __ENV__.file, line: __ENV__.line)
   end
 
   @impl true

installer/priv/templates/priv/site/index.slime
@@ -7,4 +7,4 @@ h1.title Welcome to Still
 .main
   p Hello! <br /> This is your starter to create something beautiful.
 
-  = include("_includes/footer.slime")
+  = include(@env, "_includes/footer.slime")

lib/still/compiler/template_helpers.ex
@@ -4,139 +4,130 @@ defmodule Still.Compiler.TemplateHelpers do
   preprocessor.
   """
 
-  defmacro __using__(metadata) do
-    quote do
-      alias Still.SourceFile
+  alias Still.SourceFile
 
-      alias Still.Compiler.{
-        Incremental,
-        TemplateHelpers.Link,
-        TemplateHelpers.UrlFor,
-        TemplateHelpers.LinkToCSS,
-        TemplateHelpers.LinkToJS,
-        TemplateHelpers.ContentTag,
-        TemplateHelpers.ResponsiveImage,
-        TemplateHelpers.SafeHTML,
-        TemplateHelpers.Truncate,
-        PreprocessorError
-      }
+  alias Still.Compiler.{
+    Incremental,
+    TemplateHelpers.Link,
+    TemplateHelpers.UrlFor,
+    TemplateHelpers.LinkToCSS,
+    TemplateHelpers.LinkToJS,
+    TemplateHelpers.ResponsiveImage,
+    TemplateHelpers.SafeHTML,
+    TemplateHelpers.Truncate,
+    PreprocessorError
+  }
 
-      alias __MODULE__
+  require Logger
 
-      require Logger
+  defdelegate responsive_image(_env, file, opts \\ []),
+    to: ResponsiveImage,
+    as: :render
 
-      @env unquote(metadata)
+  defdelegate url_for(relative_path), to: UrlFor, as: :render
 
-      defdelegate responsive_image(file, opts \\ []),
-        to: ResponsiveImage,
-        as: :render
+  defdelegate link_to_css(path, opts \\ []), to: LinkToCSS, as: :render
 
-      defdelegate url_for(relative_path), to: UrlFor, as: :render
+  defdelegate link_to_js(path, opts \\ []), to: LinkToJS, as: :render
 
-      defdelegate link_to_css(path, opts \\ []), to: LinkToCSS, as: :render
+  @doc """
+  Renders a file and includes it in the page, using the variables defined in `metadata`.
 
-      defdelegate link_to_js(path, opts \\ []), to: LinkToJS, as: :render
+  By default, it creates a subscription to recompile the file in which
+  `include/3` is invoked, when the target `file` changes. This behaviour
+  can be disabled by passing the `:subscribe` option as `false`.
+  """
+  def include(env, file, metadata \\ %{}, opts \\ [])
 
-      @doc """
-      Renders a file and includes it in the page, using the variables defined in `metadata`.
+  def include(env, file, metadata, opts) when is_list(metadata) do
+    include(env, file, metadata |> Enum.into(%{}), opts)
+  end
 
-      By default, it creates a subscription to recompile the file in which
-      `include/3` is invoked, when the target `file` changes. This behaviour
-      can be disabled by passing the `:subscribe` option as `false`.
-      """
-      def include(file, metadata \\ %{}, opts \\ [])
-
-      def include(file, metadata, opts) when is_list(metadata) do
-        include(file, metadata |> Enum.into(%{}), opts)
+  def include(env, file, metadata, opts) do
+    with pid when not is_nil(pid) <- Incremental.Registry.get_or_create_file_process(file),
+         subscriber <- include_subscriber(env, opts),
+         metadata <- Map.put(metadata, :dependency_chain, env[:dependency_chain] || []),
+         %SourceFile{content: content} <- Incremental.Node.render(pid, metadata, subscriber) do
+      if subscriber do
+        Incremental.Node.add_subscription(self(), file)
       end
 
-      def include(file, metadata, opts) do
-        with pid when not is_nil(pid) <- Incremental.Registry.get_or_create_file_process(file),
-             subscriber <- include_subscriber(opts),
-             metadata <- Map.put(metadata, :dependency_chain, @env[:dependency_chain]),
-             %SourceFile{content: content} <- Incremental.Node.render(pid, metadata, subscriber) do
-          if subscriber do
-            Incremental.Node.add_subscription(self(), file)
-          end
+      content
+    else
+      %PreprocessorError{} = e ->
+        raise e
 
-          content
-        else
-          %PreprocessorError{} = e ->
-            raise e
+      _ ->
+        Logger.error("File process not found for #{file}")
+        ""
+    end
+  end
 
-          _ ->
-            Logger.error("File process not found for #{file}")
-            ""
-        end
-      end
-
-      @doc """
-      Converts a relative path to an absolute one.
+  @doc """
+  Converts a relative path to an absolute one.
 
 
-      ## Examples
+  ## Examples
 
-      File paths are always relative to the root folder, but sometimes it's too
-      cumbersome, and we need to reference a file relative to the current
-      folder.
+  File paths are always relative to the root folder, but sometimes it's too
+  cumbersome, and we need to reference a file relative to the current
+  folder.
 
-      For instance, when called inside the file "blog/post/index.md":
+  For instance, when called inside the file "blog/post/index.md":
 
-          path_expand("./cover.png")
-          # "blog/post/./cover.png"
+      path_expand("./cover.png")
+      # "blog/post/./cover.png"
 
-      """
-      def path_expand(path) do
-        Path.join(Path.dirname(@env[:input_file]), path)
-      end
+  """
+  def path_expand(env, path) do
+    Path.join(Path.dirname(env[:input_file]), path)
+  end
 
-      @doc """
-      Returns the collections for the current file.
-      """
-      def get_collections(collection) do
-        Still.Compiler.Collections.get(collection, @env[:input_file])
-      end
+  @doc """
+  Returns the collections for the current file.
+  """
+  def get_collections(env, collection) do
+    Still.Compiler.Collections.get(collection, env[:input_file])
+  end
 
-      @doc """
-      Renders the link using `Still.Compiler.TemplateHelpers.Link.render/3`.
-      """
-      def link(content, opts) do
-        Link.render(content, @env, opts)
-      end
+  @doc """
+  Renders the link using `Still.Compiler.TemplateHelpers.Link.render/3`.
+  """
+  def link(env, content, opts) do
+    Link.render(env, content, opts)
+  end
 
-      @doc """
-      Safely renders the content by escaping any HTML tags.
-      """
-      def safe_html(content) do
-        SafeHTML.render(content)
-      end
+  @doc """
+  Safely renders the content by escaping any HTML tags.
+  """
+  def safe_html(content) do
+    SafeHTML.render(content)
+  end
 
-      @doc """
-      Truncates the string.
+  @doc """
+  Truncates the string.
 
-      ## Options
+  ## Options
 
-      * `escape` - apply `safe_html/1` after truncating. Defaults to `false`.
+  * `escape` - apply `safe_html/1` after truncating. Defaults to `false`.
 
-      See further supported options in `Still.TemplateHelpers.Truncate`.
-      """
-      def truncate(str, opts \\ []) do
-        truncated = Truncate.render(str, opts)
+  See further supported options in `Still.TemplateHelpers.Truncate`.
+  """
+  def truncate(str, opts \\ []) do
+    truncated = Truncate.render(str, opts)
 
-        if opts[:escape] do
-          safe_html(truncated)
-        else
-          truncated
-        end
-      end
+    if opts[:escape] do
+      safe_html(truncated)
+    else
+      truncated
+    end
+  end
 
-      defp include_subscriber(opts) do
-        if Keyword.get(opts, :subscribe, true) do
-          @env[:input_file]
-        else
-          nil
-        end
-      end
+  defp include_subscriber(env, opts) do
+    if Keyword.get(opts, :subscribe, true) do
+      env[:input_file]
+    else
+      nil
     end
   end
 end

lib/still/compiler/template_helpers/link.ex
@@ -19,20 +19,20 @@ defmodule Still.Compiler.TemplateHelpers.Link do
   `markup`. Note that this is on demand, outside
   `Still.Compiler.CompilationStage`.
   """
-  def render(opts, metadata, do: markup) do
-    preprocessor = metadata[:preprocessor]
+  def render(env, opts, do: markup) do
+    preprocessor = env[:preprocessor]
 
     %{content: content} =
       preprocessor.render(%SourceFile{
         content: markup,
-        input_file: metadata[:input_file],
-        metadata: metadata |> Enum.into(%{})
+        input_file: env[:input_file],
+        metadata: env |> Enum.into(%{})
       })
 
-    render(content, metadata, opts)
+    render(env, content, opts)
   end
 
-  def render(text, _metadata, opts) do
+  def render(_env, text, opts) do
     {url, opts} = pop_url(opts)
 
     ContentTag.render("a", text, [{:href, url} | opts])

priv/site/_layout.slime
@@ -7,7 +7,7 @@ html
     title Still
     meta charset="UTF-8"
     meta name="viewport" content="width=device-width, initial-scale=1.0"
-    = include("_includes/meta.slime")
+    = include(@env, "_includes/meta.slime")
     = link_to_js "/console.js"
     = link_to_css "/css/theme.css", media: "all"
   body

commit 67bc9ebe79abe83ecd39fcf4aaefebe749556c27

[Hotfix] Fixes an issues introduced in 987064f35cb
The refactor to Renderer left out an important piece, which is the
preprocessor in the metadata. Everything was failing but we didn't
notice because this part isn't covered by tests and the compile task is
swallowing errors.

installer/priv/templates/priv/site/_includes/footer.slime
@@ -2,5 +2,5 @@ footer
   p Relevant links:
   nav
     ul
-      li = link "Docs", to: "https://hexdocs.pm/still/"
-      li = link "Github", to: "https://github.com/still-ex/still"
+      li = link @env, "Docs", to: "https://hexdocs.pm/still/"
+      li = link @env, "Github", to: "https://github.com/still-ex/still"

lib/still/preprocessor/renderer.ex
@@ -42,6 +42,8 @@ defmodule Still.Preprocessor.Renderer do
       @extensions Keyword.fetch!(unquote(opts), :extensions)
 
       def create(%SourceFile{input_file: input_file, content: content, metadata: metadata} = file) do
+        metadata = Map.put(metadata, :preprocessor, @preprocessor)
+
         metadata[:input_file]
         |> file_path_to_module_name()
         |> create_template_renderer(content, metadata)
@@ -106,10 +108,6 @@ defmodule Still.Preprocessor.Renderer do
             import unquote(module)
           end
         end)
-      end
-
-      defp ensure_preprocessor(metadata) do
-        Map.put_new(metadata, :preprocessor, @preprocessor)
       end
     end
   end

priv/site/_includes/community.slime
@@ -9,15 +9,15 @@
         | Much ❤️ to you all.
 
       = for user <- Still.Site.Github.contributors() do
-        = link to: user.url, class: "contributor" do
+        = link @env, to: user.url, class: "contributor" do
           img src="#{user.avatar_url}"
           p
             strong = user.username
 
-      = link to: "https://github.com/still-ex/still", class: "quote" do
+      = link @env, to: "https://github.com/still-ex/still", class: "quote" do
         .mark “
         div
-          = responsive_image(@env, "_includes/stars.png", class: "stars")
+          = responsive_image("_includes/stars.png", class: "stars")
           div Breathtaking, fixed all my issues with static sites.
           div.small -- #{Still.Site.Github.stars} stargazers on GitHub
 
@@ -26,8 +26,8 @@
     .still
       h2.section-title Built With Still
 
-      = link to: "https://github.com/still-ex/still/tree/master/priv/site", class: "btn" do
+      = link @env, to: "https://github.com/still-ex/still/tree/master/priv/site", class: "btn" do
         span This website
 
-      = link to: "https://gabrielpoca.com", class: "btn" do
+      = link @env, to: "https://gabrielpoca.com", class: "btn" do
         span gabrielpoca.com

priv/site/_includes/main.slime
@@ -9,8 +9,8 @@
     p.lead A composable elixir static site builder
 
     .info
-      = link "Docs", to: "https://hexdocs.pm/still", class: "cta"
-      = link "GitHub", to: "https://github.com/still-ex/still", class: "cta"
+      = link @env, "Docs", to: "https://hexdocs.pm/still", class: "cta"
+      = link @env, "GitHub", to: "https://github.com/still-ex/still", class: "cta"
   marquee.marquee
     span Tonight on display: still (featuring elixir)
     span Tonight on display: still (featuring elixir)

priv/site/_includes/sponsors.slime
@@ -6,11 +6,11 @@
 
         p.p Still's maintenance is possible due to contributions from our sponsors:
 
-        = link to: "https://subvisual.com" do
+        = link @env, to: "https://subvisual.com" do
           = responsive_image("_includes/subvisual.png", class: "sponsor-image")
     .legal
       p.p
-        ' Still is #{link "open sourced", to: "https://github.com/still-ex/still"}
-        ' with ❤️ © #{link "Subvisual", to: "https://subvisual.com"} 2021
+        ' Still is #{link @env, "open sourced", to: "https://github.com/still-ex/still"}
+        ' with ❤️ © #{link @env, "Subvisual", to: "https://subvisual.com"} 2021
         br
-        | Hero image by #{link "UnknownArtist", to: "https://wallhaven.cc/w/13gmlv"}
+        | Hero image by #{link @env, "UnknownArtist", to: "https://wallhaven.cc/w/13gmlv"}